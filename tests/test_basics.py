8eca4e219dccb0e95e1c5c301055fb644025bb1e
ronaldoapsilva <ronaldoapsilva_@hotmail.com>
2021-09-29 18:53:06 -0300

    9.a before book

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,6 +1,7 @@
 import unittest
 from flask import current_app
 from app import create_app, db
+from app.models import User, AnonymousUser, Role, Permission
 
 
 class BasicsTestCase(unittest.TestCase):
@@ -20,3 +21,19 @@ class BasicsTestCase(unittest.TestCase):
 
     def test_app_is_testing(self):
         self.assertTrue(current_app.config['TESTING'])
+
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
